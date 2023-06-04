c22107c2de1f18f03bfef25a5d984246be45b61b
unknown <emanabufayyad@gmail.com>
2023-06-04 15:14:27 +0300

    Upload Automation Framework

---
+++ aoc_web_automation/src/test/java/com/aoc/testcases/LoginPageTest.java
@@ -0,0 +1,45 @@
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase {
+    LoginPage loginPage;
+    HomePage homePage;
+
+    public LoginPageTest() {
+        super();
+    }
+
+
+    @Test(priority = 1, Description = "Validate Login Btn is enabled when user name and password are filled")
+    public void loginPageTitleTest() {
+        String title = loginPage.validateLoginPageTitle();
+        Assert.assertEquals(title, "");
+    }
+
+    @Test(priority = 2)
+    public void logoImageTest() {
+        boolean flag = loginPage.validateCRMImage();
+        Assert.assertTrue(flag);
+    }
+
+    @Test(priority = 3)
+    public void loginTest() {
+        homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+}
